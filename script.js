348ee7fc118f59525e195f562167e0bfb7488845
Danny-lab <59095117+Danny-lab@users.noreply.github.com>
2024-11-12 13:02:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ async function iniciarCamara() {
     "Moscú": "imagenes/Moscu.jpg",
     "Pablo VI": "imagenes/Pablo.jpg",
     "La Isla": "imagenes/Isla.jpg",
+    "EL PLAYÓN": "imagenes/Playon.jpg"
   };
   
   // Configuración del trabajador de Tesseract
